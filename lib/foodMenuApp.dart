f57e63a0d8e53a9a0457210197c287b33048445a
sonalshendre <sonalshendre07@gmail.com>
2022-03-06 01:57:49 +0800

    foodmenuapp commit

--- lib/foodMenuApp.dart
+++ lib/foodMenuApp.dart
@@ -12,62 +12,93 @@ class _MenuDemoState extends State<MenuDemo> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          leading: CircleAvatar(
-            backgroundColor: Colors.white,
-            child: Image.asset('assets/foodDelivery.png'),
-          ),
-          centerTitle: true,
-          title: Text(
-            'FastDelivery!',
-            style: TextStyle(fontSize: 20, color: Colors.black),
-          ),
-          actions: [
-            Icon(Icons.phone),
-            Icon(Icons.notifications),
-          ],
-        ),
-        body: Column(
-          children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
 
-                children: [
-                  Image.asset('assets/combos.jpeg',width: 350,height: 250,),
-                  SizedBox(width: 5,),
-                  Image.asset('assets/combo2.jpeg',width: 350,height: 250,),
-                  SizedBox(width: 5,),
-                  Image.asset('assets/combos3.jpeg',width: 350,height: 250,),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: SingleChildScrollView(
+          child: Container(
+            color: Color(0xFF311B92),
+            child: Column(
               children: [
-                image_container(
-                    productname: 'Panir Capsicum pizza',
-                    productPrice: 'only at \$200'),
-                image_container(productImagepath: 'assets/burgerI.jpeg',
-                productname: 'cheese Burger',
-                  productPrice: 'at only \$150',
+                Container(
+                  color: Colors.purple,
+                  child: Center(
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/foodDelivery.png', width: 70, height: 70,),
+                        SizedBox(width: 5,),
+                        Text('FastDelivery!', style: TextStyle(fontSize: 25),),
+                        SizedBox(width: 140,),
+                        Icon(Icons.notifications),
+                      ],
+                    ),
+                  ),
                 ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Color(0xFF4527A0),
+                        Color(0xff512da8),
+                        Color(0xB59C3AB7),
+                        Color(0x9C8C3AFF),
+                      ], begin: Alignment.bottomRight,
+                        end: Alignment.topLeft,),
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/combos.jpeg',
+                          width: 300,
+                          height: 150,
+                        ),
+                        Image.asset(
+                          'assets/combo2.jpeg',
+                          width: 300,
+                          height: 150,
+                        ),
+                        Image.asset(
+                          'assets/combos3.jpeg',
+                          width: 200,
+                          height: 150,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Image.asset(name),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    image_container(
+                        productname: 'Panir Capsicum pizza',
+                        productPrice: 'only at \$200'),
+                    image_container(
+                      productImagepath: 'assets/burgerI.jpeg',
+                      productname: 'cheese Burger',
+                      productPrice: 'at only \$150',
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    image_container(
+                        productImagepath: 'assets/garlicb.jpeg',
+                        productname: 'Garlic bread',
+                        productPrice: 'just at \$70'),
+                    image_container(
+                      productImagepath: 'assets/lava.jpeg',
+                      productname: 'Choco lava Cake',
+                      productPrice: 'at only \$170',
+                    ),
+                  ],
+                )
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                image_container(
-                  productImagepath: 'assets/garlicb.jpeg',
-                    productname: 'Garlic bread',
-                    productPrice: 'just at \$70'),
-                image_container(productImagepath: 'assets/burgerI.jpeg',
-                  productname: 'cheese Burger',
-                  productPrice: 'at only \$150',
-                ),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
@@ -82,28 +113,31 @@ Widget image_container({
   double containerwidht = 150,
 }) {
   return Card(
-    elevation: 5,
-    shadowColor: Colors.lightBlue,
+    elevation: 1,
+    shadowColor: Colors.purple,
     child: Container(
-        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Color(0xff6a1b9a),
 
+        ),
+        margin: EdgeInsets.all(10),
         height: 200,
         width: 150,
-        color: Colors.grey,
+
         child: Column(
           children: [
             Image.asset(
               productImagepath,
               width: 130,
               height: 150,
-              
             ),
             Text(
               productname,
-              style: TextStyle(fontSize: 15),
+              style: TextStyle(fontSize: 15, color: Colors.white),
               maxLines: 2,
             ),
-            Text(productPrice)
+            Text(productPrice,
+              style: TextStyle(color: Colors.white),),
           ],
         )),
   );
